e3019896258074f51602739a7c87eb00f7bfd233
Jordi Casanella <jordikiter@hotmail.es>
2021-04-07 23:59:50 +0100

    First version dynamic array golang

---
+++ arrays/dynamic_array/go/dynamic_array.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"errors"
+)
+
+type ArrayDynamicer interface {
+	Init()
+	Get()
+	Push()
+	Pop()
+	DeleteAtIndex()
+	ShiftElements()
+	GetLength()
+	GetCapacity()
+}
+
+type Arrays struct {
+	data []int
+}
+
+func (a Arrays) Init() {
+	a.data = make([]int, 1)
+}
+
+func (a Arrays) Get(index int) (int, error) {
+	if index < 0 || index > len(a.data) {
+		return -1, errors.New("invalid index - can not return value")
+	}
+
+	return a.data[index], nil
+}
+
+func (a Arrays) Push(value int) int {
+	if len(a.data) == cap(a.data) {
+		// We can do something better with append
+		a.data = make([]int, cap(a.data)*2)
+	}
+
+	a.data[len(a.data)] = value
+
+	return value
+}
+
+func (a Arrays) Pop() (int, error) {
+	if (len(a.data)) > 0 {
+		value := a.data[len(a.data)-1]
+		a.data = a.data[:len(a.data)-1]
+
+		return value, nil
+	}
+
+	return -1, errors.New("empty array - can not pop value")
+}
+
+func (a Arrays) GetLength() int {
+	return len(a.data)
+}
+
+func (a Arrays) GetCapacity() int {
+	return cap(a.data)
+}
+
+func (a Arrays) DeleteAtIndex(index int) (int, error) {
+	if index < 0 || index > len(a.data) {
+		return -1, errors.New("invalid index - can not delete value")
+	}
+
+	value := a.data[index]
+	a.ShiftElements(index)
+
+	return value, nil
+}
+
+func (a Arrays) ShiftElements(index int) {
+	if index >= 0 && index < len(a.data) {
+		for idx := index; idx < len(a.data)-1; idx++ {
+			a.data[idx] = a.data[idx+1]
+		}
+
+		a.data = a.data[:len(a.data)-1]
+	}
+}
---
+++ arrays/dynamic_array/go/test_dynamic_array.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func testEmptyArray(t *testing.T) {
+	var dynamic Arrays
+	dynamic.Init()
+
+}
+
+//public void testEmptyArray() {
+//	assertEquals(0, ad.getLength(), "Length should be empty");
+//	assertEquals(1, ad.getCapacity(), "Capacity should be 1");
+//}
