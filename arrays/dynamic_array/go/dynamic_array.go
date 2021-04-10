2c9c7e65f6398f436edd2f18dbf26c8aeebb0eb1
Jordi Casanella <jordikiter@hotmail.es>
2021-04-10 23:21:09 +0100

    Fix pop and get

--- arrays/dynamic_array/go/dynamic_array.go
+++ arrays/dynamic_array/go/dynamic_array.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
 type ArrayDynamicer interface {
@@ -21,13 +20,11 @@ type Arrays struct {
 
 func New() Arrays {
 	a := Arrays{make([]int, 0, 1)}
-	fmt.Printf("len: %d capacity: %d\n", len(a.data), cap(a.data))
-
 	return a
 }
 
 func (a Arrays) Get(index int) (int, error) {
-	if index < 0 || index > len(a.data) {
+	if len(a.data) == 0 || index < 0 || index > len(a.data) {
 		return -1, errors.New("invalid index - can not return value")
 	}
 
@@ -39,7 +36,6 @@ func (a *Arrays) Push(value int) int {
 	if len(a.data) == cap(a.data) {
 		// We can do something better with append
 		newSlice = make([]int, len(a.data)+1, cap(a.data)*2)
-
 	} else {
 		newSlice = make([]int, len(a.data)+1, cap(a.data))
 	}
@@ -47,12 +43,11 @@ func (a *Arrays) Push(value int) int {
 	copy(newSlice, a.data)
 	a.data = newSlice
 	a.data[len(a.data)-1] = value
-	fmt.Println(len(a.data), cap(a.data), a.data)
 
 	return value
 }
 
-func (a Arrays) Pop() (int, error) {
+func (a *Arrays) Pop() (int, error) {
 	if (len(a.data)) > 0 {
 		value := a.data[len(a.data)-1]
 		a.data = a.data[:len(a.data)-1]
@@ -68,7 +63,6 @@ func (a Arrays) GetLength() int {
 }
 
 func (a Arrays) GetCapacity() int {
-	fmt.Printf("capacity: %d\n", cap(a.data))
 	return cap(a.data)
 }
 
--- arrays/dynamic_array/go/dynamic_array_test.go
+++ arrays/dynamic_array/go/dynamic_array_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+
 )
 
 func ValidateLengthCapacity(t *testing.T, actualLen int, expectedLen int, actualCap int, expectedCap int) {
@@ -50,4 +51,89 @@ func TestPush(t *testing.T) {
 	value = dynamic.Push(7)
 	ValidateLengthCapacity(t, dynamic.GetLength(), 7, dynamic.GetCapacity(), 8)
 	ValidateValue(t, value, 7)
+}
+
+func ValidateValueAndError(t *testing.T, actualValue int, expectedValue int, actualError error) {
+	if actualValue != expectedValue {
+		t.Errorf("Value is incorrect, got %d want %d", actualValue, expectedValue)
+	}
+
+	if actualError != nil {
+		t.Errorf("Error unexpedted error = %v", actualError)
+	}
+}
+
+func TestGet(t *testing.T) {
+	var dynamic Arrays = New()
+
+	_, error := dynamic.Get(-1)
+	if error == nil {
+		t.Errorf("Error actual = %v", error)
+	}
+
+	dynamic.Push(1)
+	value, error := dynamic.Get(0)
+	ValidateValueAndError(t, value, 1, error)
+	ValidateLengthCapacity(t, dynamic.GetLength(), 1, dynamic.GetCapacity(), 1)
+
+	dynamic.Push(2)
+	dynamic.Push(34)
+	dynamic.Push(70)
+	dynamic.Push(45)
+
+	value, error = dynamic.Get(4)
+	ValidateValueAndError(t, value, 45, error)
+	ValidateLengthCapacity(t, dynamic.GetLength(), 5, dynamic.GetCapacity(), 8)
+
+	value, error = dynamic.Get(3)
+	ValidateValueAndError(t, value, 70, error)
+
+	value, error = dynamic.Get(2)
+	ValidateValueAndError(t, value, 34, error)
+
+	value, error = dynamic.Get(1)
+	ValidateValueAndError(t, value, 2, error)
+
+	_, error = dynamic.Get(19)
+	if error == nil {
+		t.Errorf("Error actual = %v", error)
+	}
+}
+
+func TestPop(t *testing.T) {
+	var dynamic Arrays = New()
+
+	_, error := dynamic.Pop()
+	if error == nil {
+		t.Errorf("Error actual = %v", error)
+	}
+
+	actual := dynamic.Push(50)
+	expected, _ := dynamic.Pop()
+	if actual != expected {
+		t.Errorf("Error Actual %d and Expected %d are different", actual, expected)
+	}
+	ValidateLengthCapacity(t, dynamic.GetLength(), 0, dynamic.GetCapacity(), 1)
+
+	dynamic.Push(2)
+	dynamic.Push(4)
+	dynamic.Push(8)
+	dynamic.Push(16)
+	dynamic.Push(32)
+	dynamic.Push(64)
+	dynamic.Push(128)
+	ValidateLengthCapacity(t, dynamic.GetLength(), 7, dynamic.GetCapacity(), 8)
+
+	values := [...]int{128, 64, 32, 16, 8, 4, 2}
+	for _, s := range values {
+		actual, _ = dynamic.Pop()
+		if actual != s {
+			t.Errorf("Error Actual %d and Expected %d are different", actual, s)
+		}
+	}
+
+	_, error = dynamic.Pop()
+	if error == nil {
+		t.Errorf("Error actual = %v", error)
+	}
 }
