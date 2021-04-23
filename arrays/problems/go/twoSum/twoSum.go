5f723912fb00d9d546c826317041de3430e9ef04
jcasanella <jordikiter@hotmail.es>
2021-04-24 00:23:39 +0100

    Exercise

---
+++ arrays/problems/go/twoSum/twoSum.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+func twoSum(nums []int, target int) []int {
+	for idx := 0; idx < len(nums)-1; idx++ {
+
+		if nums[idx] >= target {
+			break
+		}
+
+		expected := target - nums[idx]
+
+		for idx2 := idx + 1; idx2 < len(nums); idx2++ {
+
+			if nums[idx2] == expected {
+				output := make([]int, 2, 2)
+				output[0] = idx
+				output[1] = idx2
+				return output
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+
+}
