be18488542e6d0cec1d090daa43f07722487fee6
Jordi Casanella <jordikiter@hotmail.es>
2021-05-25 23:56:41 +0100

    Add test

---
+++ maps/hashmap/go/hashmap_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"data_structures/data/structures/hashmap"
+	"testing"
+
+)
+
+// Add(key string, value int)
+// Remove(key string) bool
+// Get(key string) (int, bool)
+// GetKeys() []string
+// Size() uint
+
+func TestEmpty(t *testing.T) {
+	var hm *hashmap.Hashmap = hashmap.NewHashmap(10)
+
+	// Must be empty
+	numElems := hm.Size()
+	if numElems != 0 {
+		t.Errorf("Expected size is %d but actual is 0\n", numElems)
+	}
+
+	// Get Keys
+	keys := hm.GetKeys()
+	if len(keys) != 0 {
+		t.Errorf("Expected size is %d but actual is 0\n", numElems)
+	}
+
+	// Get element
+	if value, error := hm.Get("key1"); !error {
+		t.Errorf("Expected an error, but no error and returned value %d\n", value)
+	}
+}
