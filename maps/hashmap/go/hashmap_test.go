a3094e016a27875e55f6f485866ad7e1d8aedd62
Jordi Casanella <jordikiter@hotmail.es>
2021-05-29 11:30:44 +0100

    Adding more unit test to hashmap

--- maps/hashmap/go/hashmap_test.go
+++ maps/hashmap/go/hashmap_test.go
@@ -16,15 +16,13 @@ func TestEmpty(t *testing.T) {
 	var hm *hashmap.Hashmap = hashmap.NewHashmap(10)
 
 	// Must be empty
-	numElems := hm.Size()
-	if numElems != 0 {
+	if numElems := hm.Size(); numElems != 0 {
 		t.Errorf("Expected size is %d but actual is 0\n", numElems)
 	}
 
 	// Get Keys
-	keys := hm.GetKeys()
-	if len(keys) != 0 {
-		t.Errorf("Expected size is %d but actual is 0\n", numElems)
+	if keys := hm.GetKeys(); len(keys) != 0 {
+		t.Errorf("Expected size is %d but actual is 0\n", len(keys))
 	}
 
 	// Get element
@@ -34,5 +32,76 @@ func TestEmpty(t *testing.T) {
 
 	if removed := hm.Remove("key1"); removed {
 		t.Errorf("Removed when no values in the hashMap")
+	}
+}
+
+func TestAddElem(t *testing.T) {
+	var hm *hashmap.Hashmap = hashmap.NewHashmap(10)
+
+	hm.Add("key1", 10)
+	if numElems := hm.Size(); numElems != 1 {
+		t.Errorf("Expected size 1 but is %d\n", numElems)
+	}
+
+	// Must be one
+	if numElems := hm.Size(); numElems != 1 {
+		t.Errorf("Expected size is %d but actual is 1\n", numElems)
+	}
+
+	// Get element
+	if value, error := hm.Get("key1"); value != 10 || error {
+		t.Errorf("Expected value 10 but get %d and noError but was %t\n", value, error)
+	}
+
+	// Get keys
+	if keys := hm.GetKeys(); len(keys) != 1 {
+		t.Errorf("Expected 1 key but got %d\n", len(keys))
+	}
+	if keys := hm.GetKeys(); keys[0] != "key1" {
+		t.Errorf("Expected key1 key but got %v\n", keys[0])
+	}
+
+	// Overwrite value
+	hm.Add("key1", 15)
+	if numElems := hm.Size(); numElems != 1 {
+		t.Errorf("Expected size 1 but is %d\n", numElems)
+	}
+
+	// Must be one
+	if numElems := hm.Size(); numElems != 1 {
+		t.Errorf("Expected size is %d but actual is 1\n", numElems)
+	}
+
+	// Get element
+	if value, error := hm.Get("key1"); value != 15 || error {
+		t.Errorf("Expected value 15 but get %d and noError but was %t\n", value, error)
+	}
+
+	// Get keys
+	if keys := hm.GetKeys(); len(keys) != 1 {
+		t.Errorf("Expected 1 key but got %d\n", len(keys))
+	}
+	if keys := hm.GetKeys(); keys[0] != "key1" {
+		t.Errorf("Expected key1 key but got %v\n", keys[0])
+	}
+
+	// Remove Element
+	if removed := hm.Remove("key1"); !removed {
+		t.Errorf("Value not removed\n")
+	}
+
+	// Must be empty
+	if numElems := hm.Size(); numElems != 0 {
+		t.Errorf("Expected size is %d but actual is 0\n", numElems)
+	}
+
+	// Get keys - empty
+	if keys := hm.GetKeys(); len(keys) != 0 {
+		t.Errorf("Expected 0 key but got %d\n", len(keys))
+	}
+
+	// Get element
+	if value, error := hm.Get("key1"); !error {
+		t.Errorf("Expected error but got noError and value %d\n", value)
 	}
 }
